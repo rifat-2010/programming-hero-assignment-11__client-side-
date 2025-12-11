b5f096e46c737dd66a535154101ae73ac0cd8d8d
Rifat240510 <rifat240510@gmail.com>
2025-12-11 16:40:31 +0600

    books_page > search bar implement

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import { useLoaderData } from 'react-router';
 import BookCart from '../components/BookCart';
+import { FaSearch } from "react-icons/fa";
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 
 const Books = () => {
   const data = useLoaderData();
 //   console.log(data);
+const [books, setBooks] = useState([]);
+const [search, setSearch] = useState("");
+const [loading, setLoading] = useState(false);
+
+// Fetch books from server
+const loadBooks = async (query) => {
+  console.log(query)
+  setLoading(true);
+  const res = await fetch(`http://localhost:3000/search?search=${query}`);
+  const data = await res.json();
+  // console.log(data);
+  setBooks(data);
+  setLoading(false);
+};
+
+
+// Load all books on first render
+useEffect(() => {
+  loadBooks(search);
+}, [search]);
+
+
 
   return (
     <div>
@@ -20,10 +46,34 @@ const Books = () => {
     </p>
   </div>
     </div>
+    
+
+      {/* Search + Count Row */}
+<div className="flex items-center justify-between w-11/12 mx-auto mb-8">
+
+  {/* Search Box */}
+  <div className="flex items-center gap-3">
+    <FaSearch size={28} className="text-blue-800" />
+    <input
+  type="search"
+  placeholder="Search books..."
+  className="border-2 border-blue-800 rounded-xl px-3 py-2 focus:outline-none"
+  onChange={(e) => setSearch(e.target.value)}
+    />
+
+  </div>
+
+  {/* Count Text */}
+  <p className="text-xl font-semibold text-gray-800 dark:text-gray-200">
+    Total Books: {data.length}
+  </p>
+</div>
+
+            
 
       {/* Grid Layout */}
-    <div className="w-11/12 mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-    {data.map((book) => (
+    <div className="w-11/12 mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mb-5">
+    {books.map((book) => (
               <div
                 key={book._id}
               >
